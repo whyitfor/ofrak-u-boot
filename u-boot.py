468762216d4ce506ea27ce70bcf85bcfa87a40ac
Dan Pesce <dan@redballoonsecurity.com>
2024-11-06 17:51:30 -0500

    untested ghidra gzf code

--- u-boot.py
+++ u-boot.py
@@ -2,10 +2,34 @@ import argparse
 
 from ofrak import OFRAK
 from ofrak import OFRAKContext
-async def main(ofrak_context: OFRAKContext, file: str, ghidra_project: str)
+from ofrak.core import *
+from ofrak_ghidra.ghidra_model import GhidraProject
+from ofrak_ghidra.components.ghidra_analyzer import (
+    GhidraProjectConfig,
+    GhidraProjectAnalyzer,
+)
+
+
+async def main(ofrak_context: OFRAKContext, file: str, ghidra_project: str):
     resource = await ofrak_context.create_root_resource_from_file(file)
 
     await resource.flush_data_to_disk(f"{file}.modified")
+    program_attributes = ProgramAttributes(
+        InstructionSet.ARM,
+        bit_width=BitWidth.BIT_32,
+        endianness=Endianness.LITTLE_ENDIAN,
+    )
+    resource.add_attributes(program_attributes)
+    await resource.save()
+    resource.add_tag(GhidraProject)
+    await resource.save()
+    config = GhidraProjectConfig(
+        ghidra_zip_file=ghidra_project, name="ghidra_project", use_existing=True
+    )
+    await resource.run(GhidraProjectAnalyzer, config=config)
+    complex_blocks = await resource.get_descendants_as_view(
+        v_type=ComplexBlock, r_filter=ResourceFilter(tags=(ComplexBlock,))
+    )
 
 
 if __name__ == "__main__":
@@ -16,5 +40,6 @@ if __name__ == "__main__":
 
     o = OFRAK()
     import ofrak_ghidra
+
     o.discover(ofrak_ghidra)
     o.run(main)

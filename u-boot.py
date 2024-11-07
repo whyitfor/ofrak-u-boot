dc0edc41e3ab9da1921509ecb64120177497d58a
Dan Pesce <dan@redballoonsecurity.com>
2024-11-06 19:05:36 -0500

    extract complex blocks

--- u-boot.py
+++ u-boot.py
@@ -8,36 +8,45 @@ from ofrak_ghidra.components.ghidra_analyzer import (
     GhidraProjectConfig,
     GhidraProjectAnalyzer,
 )
+from ofrak_ghidra.components.blocks.unpackers import GhidraCodeRegionUnpacker
 
 
-async def main(ofrak_context: OFRAKContext, file: str, ghidra_project: str):
-    resource = await ofrak_context.create_root_resource_from_file(file)
+async def main(ofrak_context: OFRAKContext):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", default="assets/u-boot.bin")
+    parser.add_argument("--gzf", default="assets/u-boot.bin.gzf")
+    args = parser.parse_args()
+    resource = await ofrak_context.create_root_resource_from_file(args.file)
 
-    await resource.flush_data_to_disk(f"{file}.modified")
+    await resource.flush_data_to_disk(f"{args.file}.modified")
     program_attributes = ProgramAttributes(
         InstructionSet.ARM,
         bit_width=BitWidth.BIT_32,
-        endianness=Endianness.LITTLE_ENDIAN,
+        endianness=Endianness.LITTLE_ENDIAN,\
+        sub_isa=None,
+        processor=None,
     )
     resource.add_attributes(program_attributes)
     await resource.save()
     resource.add_tag(GhidraProject)
     await resource.save()
     config = GhidraProjectConfig(
-        ghidra_zip_file=ghidra_project, name="ghidra_project", use_existing=True
+        ghidra_zip_file=args.gzf, name="ghidra_project", use_existing=True
     )
     await resource.run(GhidraProjectAnalyzer, config=config)
+    data_length = await resource.get_data_length()
+    resource.add_view(CodeRegion(
+        virtual_address=0,
+        size=data_length,
+    ))
+    await resource.save()
+    await resource.run(GhidraCodeRegionUnpacker)
     complex_blocks = await resource.get_descendants_as_view(
         v_type=ComplexBlock, r_filter=ResourceFilter(tags=(ComplexBlock,))
     )
-
+    print(complex_blocks)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file", default="assets/u-boot.bin")
-    parser.add_argument("--gzf", default="assets/u-boot.bin.gzf")
-    args = parser.parse_args()
-
     o = OFRAK()
     import ofrak_ghidra
 
